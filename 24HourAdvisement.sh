1d80dffe3f43135ff041da915288e03d5a498937
Ocean <dvsocean@icloud.com>
2018-08-23 12:56:58 -0700

    mvn build console output

--- 24HourAdvisement.sh
+++ 24HourAdvisement.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /Users/danielocean/Documents/AUTOMATION-MAIN/Automation
+cd /Users/danielocean/Documents/path/to/test/repo
 /usr/local/apache-maven/apache-maven-3.5.0/bin/mvn clean test >> ~/Desktop/24HourAdvisementConsoleOutput.txt
 
 #Using this shell script will provide a clean console output in a separate text file
