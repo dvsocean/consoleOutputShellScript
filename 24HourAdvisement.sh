273348e10607590ca63649c941ccbc70a153f662
Ocean <dvsocean@icloud.com>
2018-08-24 07:02:24 -0700

    mods

--- 24HourAdvisement.sh
+++ 24HourAdvisement.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 cd /Users/danielocean/Documents/path/to/test/repo
-/usr/local/apache-maven/apache-maven-3.5.0/bin/mvn clean test >> ~/Desktop/24HourAdvisementConsoleOutput.txt
+/usr/local/apache-maven/apache-maven-3.5.0/bin/mvn clean test >> ~/Desktop/ChooseFileNameForOutput.txt
 
-#Using this shell script will provide a clean console output in a separate text file
